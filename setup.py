e3b14af14a1818af6d8644219cab315936105659
TheGenerativeGeneration <6620807+thegenerativegeneration@users.noreply.github.com>
2024-07-16 22:25:31 +0200

    use custom Gliner

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     },
     install_requires=[
         'spacy>=3.0.0',
-        'gliner>=0.2.0',
+        'gliner @ git+https://github.com/thegenerativegeneration/GLiNER.git',
         'seaborn',
         'matplotlib',
         'numpy'
